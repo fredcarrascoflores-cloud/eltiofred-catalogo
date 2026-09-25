fdca4141c26635305db24ea75383cfd57f12d404
¨fredcarrascoflores-cloud΅ <fredcarrascoflores@gmail.com>
2026-09-25 00:55:57 -0500

    Actualizar catalogo

--- catalogo_items.js
+++ catalogo_items.js
@@ -1,11 +1,83 @@
 window.catalogItems = [
   {
+    "name": "Arcana Jugger",
+    "hero": "",
+    "price": 73,
+    "type": "Tradeable",
+    "stock": 3,
+    "image": "assets/arcana_jugger_73_stock_3.jpeg"
+  },
+  {
+    "name": "Arcana Lc",
+    "hero": "",
+    "price": 70,
+    "type": "Tradeable",
+    "stock": 1,
+    "image": "assets/arcana_lc_70.jpeg"
+  },
+  {
+    "name": "Arcana Lina",
+    "hero": "",
+    "price": 75,
+    "type": "Tradeable",
+    "stock": 1,
+    "image": "assets/arcana_lina_75.jpeg"
+  },
+  {
+    "name": "Arcana Mayden Full",
+    "hero": "",
+    "price": 5,
+    "type": "Tradeable",
+    "stock": 5,
+    "image": "assets/arcana_mayden full_5_stock_5.jpeg"
+  },
+  {
+    "name": "Arcana Monky",
+    "hero": "",
+    "price": 70,
+    "type": "Tradeable",
+    "stock": 5,
+    "image": "assets/arcana_monky_70_stock_5.jpeg"
+  },
+  {
+    "name": "Arcana Phantom Full",
+    "hero": "",
+    "price": 75,
+    "type": "Tradeable",
+    "stock": 5,
+    "image": "assets/arcana_phantom full_75_stock_5.jpeg"
+  },
+  {
+    "name": "Arcana Pudge Full",
+    "hero": "",
+    "price": 68,
+    "type": "Tradeable",
+    "stock": 5,
+    "image": "assets/arcana_pudge full_68_stock_5.jpeg"
+  },
+  {
     "name": "Arcana Rubick Full",
     "hero": "",
     "price": 97,
     "type": "Tradeable",
     "stock": 5,
     "image": "assets/arcana_rubick full_97_stock 5.jpeg"
+  },
+  {
+    "name": "Arcana Shadow F",
+    "hero": "",
+    "price": 70,
+    "type": "Tradeable",
+    "stock": 1,
+    "image": "assets/arcana_shadow f_70.jpeg"
+  },
+  {
+    "name": "Arcana Sky Full",
+    "hero": "",
+    "price": 65,
+    "type": "Tradeable",
+    "stock": 1,
+    "image": "assets/arcana_sky full_65.jpeg"
   },
   {
     "name": "Arcana Wk",
@@ -24,6 +96,14 @@ window.catalogItems = [
     "image": "assets/arcana_wr_315_stock_5.jpeg"
   },
   {
+    "name": "Arcana Zeus",
+    "hero": "",
+    "price": 75,
+    "type": "Tradeable",
+    "stock": 1,
+    "image": "assets/arcana_zeus_75.jpeg"
+  },
+  {
     "name": "Arcano Tb",
     "hero": "",
     "price": 100,
@@ -40,12 +120,28 @@ window.catalogItems = [
     "image": "assets/carmesi wr_560_stock_7.jpeg"
   },
   {
+    "name": "Guitarra Doom",
+    "hero": "",
+    "price": 75,
+    "type": "Tradeable",
+    "stock": 1,
+    "image": "assets/guitarra_doom_75.jpeg"
+  },
+  {
     "name": "Intergalactic Obliterator",
     "hero": "",
     "price": 315,
     "type": "Tradeable",
     "stock": 10,
     "image": "assets/INTERGALACTIC OBLITERATOR__315__10.jpeg"
+  },
+  {
+    "name": "Serrucho Pudge",
+    "hero": "",
+    "price": 65,
+    "type": "Tradeable",
+    "stock": 1,
+    "image": "assets/serrucho_pudge_65.jpeg"
   },
   {
     "name": "Set Monkey",
